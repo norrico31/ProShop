11ce53b4aaf09d254e2495cfe2c0aa72bb20406b
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-04-22 17:12:18 +0800

    create a Product component and use in HomeScreen component

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
+import { Product } from '../components/Product'
+
+export const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map(product => (
+                    <Col sm={12} md={6} lg={4} xl={3} key={product.id}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
