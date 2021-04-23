70ab65128428be54827e06b6c85b2a05ae45eb1c
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-04-23 11:23:23 +0800

    react-router-dom and react-router-bootstrap installed

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import { Card } from "react-bootstrap"
 import { Rating } from "./Rating"
 
@@ -6,13 +7,13 @@ export const Product = props => {
     return (
         <>
             <Card className='my-3 p-3 rounded'>
-                <a href={`/product/${props.product._id}`}>
+                <Link to={`/product/${props.product._id}`}>
                     <Card.Img src={props.product.image} variant='top' />
-                </a>
+                </Link>
                 <Card.Body>
-                    <a href={`/product/${props.product._id}`}>
+                    <Link to={`/product/${props.product._id}`}>
                         <Card.Title as='div'><strong>{props.product.name}</strong></Card.Title>
-                    </a>
+                    </Link>
                     <Card.Text as='div'>
                         <Rating value={props.product.rating} text={`${props.product.numReviews} reviews`}/>
                     </Card.Text>
