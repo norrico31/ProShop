fa4e7833cb67f5dc4b49ae6ebb7d377c80ee21f4
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-04-28 19:44:58 +0800

    cart action, reducer and constants created and setup a localStorage to save the items in storage in initial render. getState used in addToCart function

--- frontend/src/store/actions/cartActions.js
+++ frontend/src/store/actions/cartActions.js
@@ -2,21 +2,17 @@ import axios from 'axios'
 import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../constants/cartConstants'
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
-    try {
-        const {data} = await axios.get(`/api/products/${productId}`)
-        dispatch({
-            type: CART_ADD_ITEM,
-            payload: {
-                product: data._id,
-                name: data.name,
-                image: data.image,
-                price: data.price,
-                countInStock: data.countInStock,
-                qty
-            }
-        })
-        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-    } catch (error) {
-        
-    }
+    const {data} = await axios.get(`/api/products/${productId}`)
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty
+        }
+    })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
