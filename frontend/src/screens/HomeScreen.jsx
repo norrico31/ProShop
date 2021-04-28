02de5a8d53b75b38a91ae71aa32a62dcc7243425
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-04-28 14:55:22 +0800

    remove the arrays in the productReducer and dispatch the listProducts actions to fetch all products in HomeScreen using useDispatch and useSelector

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,28 +1,26 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import {Product} from '../components/Product'
+import { listProducts } from '../store/actions/productActions'
 
 export const HomeScreen = () => {
-    const [products, productsSet] = useState([])
-
+    const productList = useSelector(function(state) {return state.productList})
+    const dispatch = useDispatch()
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products')
-            productsSet(res.data)
-        }
-        fetchProducts()
-    } ,[])
+        dispatch(listProducts())
+    } ,[dispatch])
+    console.log(productList.products)
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
+            {productList.loading ? <div>Loading</div>: productList.error ? <h3>{productList.error}</h3> : <Row>
+                {productList.products.map(product => (
                     <Col sm={12} md={6} lg={4} xl={3} key={product._id} >
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+            </Row>} 
         </>
     )
 }
