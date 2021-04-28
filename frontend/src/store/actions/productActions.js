70f294b9073c2984f5bfcbfe19afc10f10338e4d
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-04-28 16:54:23 +0800

    product details reducer, action and constants created and used it in ProductScreen to fetch single product using product id from match.params.id

--- frontend/src/store/actions/productActions.js
+++ frontend/src/store/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants'
 
 export const listProducts = () => async dispatch => {
     dispatch({type: PRODUCT_LIST_REQUEST})
@@ -8,5 +8,22 @@ export const listProducts = () => async dispatch => {
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: res.data})
     } catch (error) {
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message})
+    }
+}
+
+export const listProductDetails = productId => async dispatch => {
+    dispatch({
+        type: PRODUCT_DETAILS_REQUEST
+    })
+    try {
+        const res = await axios.get('/api/products/' + productId)
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS, 
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
     }
 }
--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux'
-import { productListReducer } from './productReducers'
+import { productDetailsReducer, productListReducer } from './productReducers'
 
 export const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer
 })
