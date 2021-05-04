d0b6e46e03192f2673218e91563442b72a16c7fc
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-05-04 13:45:03 +0800

    userLogin reducer and actions

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 import { cartReducer } from './cartReducers'
 import { productDetailsReducer, productListReducer } from './productReducers'
+import {userLoginReducer} from './userReducers'
 
 export const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,9 +4,13 @@ import reduxThunk from 'redux-thunk'
 import {reducer} from './reducers'
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
     cart: { cartItems: cartItemsFromLocalStorage },
+    userLogin: {
+        userInfo: userInfoFromLocalStorage
+    }
 }
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(reduxThunk)))
 
