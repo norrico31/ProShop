3810427b559dc6bb776389794e38d9d41c10914e
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-05-09 19:05:45 +0800

    check-out steps component into ShippingScreen

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,6 +3,7 @@ import {Form, Button} from 'react-bootstrap'
 import {useSelector, useDispatch} from 'react-redux'
 import {FormContainer} from '../components/FormContainer'
 import {saveShippingAddress} from '../store/actions/cartActions'
+import {CheckoutSteps} from '../components/CheckoutSteps'
 
 export const ShippingScreen = ({history}) => {
     const {shippingAddress} = useSelector(function({cart}){return cart})
@@ -19,6 +20,7 @@ export const ShippingScreen = ({history}) => {
     }
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
