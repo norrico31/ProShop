965043fb34077d6ba70cc8496fd3f39878e0f0f9
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-05-10 14:06:56 +0800

    OrderScreen and functionality created frontend

--- frontend/src/store/reducers/orderReducers.js
+++ frontend/src/store/reducers/orderReducers.js
@@ -22,7 +22,7 @@ export const orderCreateReducer = (state = {}, action) => {
     }
 }
 
-export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {}}, {type, payload}) => {
+export const orderDetailsReducer = (state = {loading: true, orderItems: [], shippingAddress: {}}, {type, payload}) => {
     switch (type) {
         case ORDER_DETAILS_REQUEST:
             return {
