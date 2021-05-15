b14bb8659da8478a503deea0bf30a41fd7b39e08
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-05-15 17:56:30 +0800

    bug fixed for user state and order state

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -1,4 +1,5 @@
-import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL} from '../constants/userConstants'
+import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAILS_RESET} from '../constants/userConstants'
+import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
 import axios from 'axios'
 
 export const login = (email, password) => async dispatch => {
@@ -97,4 +98,6 @@ export const updateUserProfile = user => async (dispatch, getState) => {
 export const logout = () => async dispatch => {
     localStorage.removeItem('userInfo')
     dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
+    dispatch({type: ORDER_LIST_MY_RESET})
 }
