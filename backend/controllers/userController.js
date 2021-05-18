00329da8d214e79c3c0b6480348b8d49fbbcf887
Norrico Gerald Biason <norricobiason31@gmail.com>
2021-05-18 19:36:50 +0800

    get and update/patch user by the admin using user id. backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -109,3 +109,38 @@ export const deleteUserByAdmin = AsyncHandler(async (req, res) => {
       throw new Error('User not found')
    }
 })
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+export const getUserById = AsyncHandler(async (req, res) => {
+   const user = await User.findById(req.params.id).select('-password')
+   if (user) {
+      res.json(user)
+   } else {
+      res.status(404)
+      throw new Error('User not found')
+   }
+})
+
+// @desc    Update user profile by admin
+// @route   Patch /api/users/:id
+// @access  Private
+export const updateUserByAdmin = AsyncHandler(async (req, res) => {
+   const user = await User.findById(req.params.id)
+   if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      user.isAdmin = req.body.isAdmin
+      const updatedUser = await user.save()
+      res.json({
+         _id: updatedUser._id, 
+         name: updatedUser.name,
+         email: updatedUser.email, 
+         isAdmin: updatedUser.isAdmin, 
+      })
+   } else {
+      res.status(404)
+      throw new Error('User not found')
+   }
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import {authUser, deleteUserByAdmin, getUserProfile, getUsersByAdmin, registerUser, updateUserProfile} from '../controllers/userController.js'
+import {authUser, deleteUserByAdmin, getUserById, getUserProfile, getUsersByAdmin, registerUser, updateUserByAdmin, updateUserProfile} from '../controllers/userController.js'
 import {admin, protect} from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsersByAdmin)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).patch(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUserByAdmin)
+router.route('/:id').delete(protect, admin, deleteUserByAdmin).get(protect, admin, getUserById).patch(protect, admin, updateUserByAdmin)
 
 export default router
